e08eaeaccebc24d4c2ad4c87e55e016c665338c7
Irvin Chan <irvin6269@gmail.com>
2021-01-14 13:35:27 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GitpythonTuto
+
+This repository contains an example on how to automate some git process.
+
+With this code we will able to automate the next steps.
+
+- Clone the repository.
+- If the repo was cloned before, we will pull the changes from the master branch.
+- Add changes to our file.txt file.
+- Commit the changes.
+- Push the changes.
+- Create a PR.
+- Merge the changes.
+
+The repository that is modified by this script can be found on [this link](https://github.com/ICCanche/GitpythonTuto)
---
+++ __main__.py
@@ -0,0 +1,62 @@
+import os
+from git import Repo
+import time
+
+local_repo_directory = os.path.join(os.getcwd(), 'GitpythonTuto')
+destination = 'main'
+
+
+def clone_repo():
+    if os.path.exists(local_repo_directory):
+        print("Directory exists, pulling changes from main branch")
+        repo = Repo(local_repo_directory)
+        origin = repo.remotes.origin
+        origin.pull(destination)
+    else:
+        print("Directory does not exists, cloning")
+        Repo.clone_from("git@github.com:ICCanche/GitpythonTuto.git",
+                        local_repo_directory, branch=destination)
+
+
+def chdirectory(path):
+    os.chdir(path)
+
+
+def update_file():
+    print("Modifying the file")
+    chdirectory(local_repo_directory)
+    opened_file = open("file.txt", 'a')
+    opened_file.write("{0} added at {1} \n".format(
+        "I am a new string", str(time.time())))
+
+
+def add_and_commit_changes(repo):
+    print("Commiting changes")
+    repo.git.add(update=True)
+    repo.git.commit("-m", "Adding a new line to the file.text file")
+
+
+def push_changes(repo):
+    print("Push changes")
+    repo.git.push("--set-upstream", 'origin', destination)
+
+
+def main():
+
+    repo = Repo(local_repo_directory)
+
+    # clone the repository
+    clone_repo()
+
+    # update file
+    update_file()
+
+    # add and commit changes
+    add_and_commit_changes(repo)
+
+    # push changes
+    push_changes(repo)
+
+
+if __name__ == "__main__":
+    main()
